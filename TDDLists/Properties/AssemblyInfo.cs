ecadf4930e14d8e7b6cb54df40d59b29f7c49949
John Reese <jpreese@mtu.edu>
2015-09-19 13:50:01 -0400

    Rename assembly title

--- TDDLists/Properties/AssemblyInfo.cs
+++ TDDLists/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("TDDLists")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TEAM OS")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("TDDLists")]
 [assembly: AssemblyCopyright("Copyright © TEAM OS 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
